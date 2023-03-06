18812b33ab0c731ec7f0ee2081b9d71839aa6736
alexmunoz1 <amunoz@filadd.com>
2023-03-06 16:59:44 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, // for guild related things
+    GatewayIntentBits.GuildMembers, // for guild members related things
+    GatewayIntentBits.GuildEmojisAndStickers, // for manage emojis and stickers
+    GatewayIntentBits.GuildIntegrations, // for discord Integrations
+    GatewayIntentBits.GuildWebhooks, // for discord webhooks
+    GatewayIntentBits.GuildInvites, // for guild invite managing
+    GatewayIntentBits.GuildVoiceStates, // for voice related things
+    GatewayIntentBits.GuildPresences, // for user presence things
+    GatewayIntentBits.GuildMessages, // for guild messages things
+    GatewayIntentBits.GuildMessageReactions, // for message reactions things
+    GatewayIntentBits.GuildMessageTyping, // for message typing things
+    GatewayIntentBits.DirectMessages, // for dm messages
+    GatewayIntentBits.DirectMessageReactions, // for dm message reaction
+    GatewayIntentBits.DirectMessageTyping, // for dm message typinh
+    GatewayIntentBits.MessageContent, // enable if you need message content things
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.GuildMember,
+    Partials.User,
+    Partials.ThreadMember,
+  ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
